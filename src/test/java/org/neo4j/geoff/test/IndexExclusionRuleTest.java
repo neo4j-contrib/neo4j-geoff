6f1ea11423dec35fc85e7c369a358695b347c80d
Nigel Small <nigel@nigelsmall.name>
2011-12-22 21:27:40 +0000

    completed all happy path tests

--- src/test/java/org/neo4j/geoff/test/IndexExclusionRuleTest.java
+++ src/test/java/org/neo4j/geoff/test/IndexExclusionRuleTest.java
@@ -20,7 +20,20 @@
 package org.neo4j.geoff.test;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.geoff.*;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.PropertyContainer;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.index.Index;
 import org.neo4j.test.ImpermanentGraphDatabase;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class IndexExclusionRuleTest {
 
@@ -29,6 +42,95 @@ public class IndexExclusionRuleTest {
 	@Before
 	public void setUp() throws Exception {
 		db = new ImpermanentGraphDatabase();
+	}
+
+	@Test
+	public void testNodeIndexExclusionRule() throws Exception {
+		String source = "(A)!=|People| {\"name\": \"Alice\"}";
+		Rule rule = Rule.from(source);
+		assertNotNull(rule);
+		assertEquals("N'I", rule.getDescriptor().getPattern());
+		assertTrue(rule.getDescriptor().getToken(0) instanceof NodeToken);
+		assertTrue(rule.getDescriptor().getToken(2) instanceof IndexToken);
+		NodeToken nodeToken = (NodeToken) rule.getDescriptor().getToken(0);
+		assertEquals(Token.Type.NODE, nodeToken.getTokenType());
+		assertTrue(nodeToken.hasName());
+		assertEquals("A", nodeToken.getName());
+		IndexToken indexToken = (IndexToken) rule.getDescriptor().getToken(2);
+		assertEquals(Token.Type.INDEX, indexToken.getTokenType());
+		assertTrue(indexToken.hasName());
+		assertEquals("People", indexToken.getName());
+		assertTrue(rule.getData().containsKey("name"));
+		assertEquals("Alice", rule.getData().get("name"));
+	}
+
+	@Test
+	public void testRelationshipIndexExclusionRule() throws Exception {
+		String source = "[R]!=|People| {\"name\": \"Alice\"}";
+		Rule rule = Rule.from(source);
+		assertNotNull(rule);
+		assertEquals("R'I", rule.getDescriptor().getPattern());
+		assertTrue(rule.getDescriptor().getToken(0) instanceof RelToken);
+		assertTrue(rule.getDescriptor().getToken(2) instanceof IndexToken);
+		RelToken relToken = (RelToken) rule.getDescriptor().getToken(0);
+		assertEquals(Token.Type.REL, relToken.getTokenType());
+		assertTrue(relToken.hasName());
+		assertEquals("R", relToken.getName());
+		assertFalse(relToken.hasType());
+		IndexToken indexToken = (IndexToken) rule.getDescriptor().getToken(2);
+		assertEquals(Token.Type.INDEX, indexToken.getTokenType());
+		assertEquals(true, indexToken.hasName());
+		assertEquals("People", indexToken.getName());
+		assertTrue(rule.getData().containsKey("name"));
+		assertEquals("Alice", rule.getData().get("name"));
+	}
+
+	@Test
+	public void testLoadingNodeIndexExclusionRuleWithLoadParameter() throws Exception {
+		// perform first call to inject node
+		String source =
+				"(A) {\"given-names\": \"Alice\", \"family-name\": \"Allison\"}\n" +
+				"(A)<=|People| {\"name\": \"Allison, Alice\"}\n" +
+				"";
+		Map<String, PropertyContainer> out = GEOFF.loadIntoNeo4j(new StringReader(source), db, null);
+		assertNotNull(out);
+		// build params for second call from output of first
+		Node node = (Node) out.get("(A)");
+		assertNotNull(node);
+		Map<String, PropertyContainer> params = new HashMap<String, PropertyContainer>(1);
+		params.put("A", node);
+		// make second call to add index entry
+		source = "(A)!=|People| {\"name\": \"Allison, Alice\"}";
+		GEOFF.loadIntoNeo4j(new StringReader(source), db, params);
+		// check results
+		assertTrue(db.index().existsForNodes("People"));
+		Index<Node> people = db.index().forNodes("People");
+		assertFalse(people.get("name", "Allison, Alice").hasNext());
+	}
+
+	@Test
+	public void testLoadingRelationshipIndexExclusionRuleWithLoadParameter() throws Exception {
+		// perform first call to inject relationship
+		String source =
+				"(A) {\"name\": \"Alice\"}\n" +
+				"(B) {\"name\": \"Bob\"}\n" +
+				"(A)-[R:KNOWS]->(B) {\"since\": 1977}\n" +
+				"[R]<=|Friends| {\"names\": \"Alice & Bob\"}\n" +
+				"";
+		Map<String, PropertyContainer> out = GEOFF.loadIntoNeo4j(new StringReader(source), db, null);
+		assertNotNull(out);
+		// build params for second call from output of first
+		Relationship rel = (Relationship) out.get("[R]");
+		assertNotNull(rel);
+		Map<String, PropertyContainer> params = new HashMap<String, PropertyContainer>(1);
+		params.put("R", rel);
+		// make second call to add index entry
+		source = "[R]!=|Friends| {\"names\": \"Alice & Bob\"}";
+		GEOFF.loadIntoNeo4j(new StringReader(source), db, params);
+		// check results
+		assertTrue(db.index().existsForRelationships("Friends"));
+		Index<Relationship> friends = db.index().forRelationships("Friends");
+		assertFalse(friends.get("names", "Alice & Bob").hasNext());
 	}
 
 }
--- src/test/java/org/neo4j/geoff/test/RelationshipExclusionRuleTest.java
+++ src/test/java/org/neo4j/geoff/test/RelationshipExclusionRuleTest.java
@@ -20,7 +20,17 @@
 package org.neo4j.geoff.test;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.geoff.*;
+import org.neo4j.graphdb.*;
 import org.neo4j.test.ImpermanentGraphDatabase;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class RelationshipExclusionRuleTest {
 
@@ -29,6 +39,191 @@ public class RelationshipExclusionRuleTest {
 	@Before
 	public void setUp() throws Exception {
 		db = new ImpermanentGraphDatabase();
+	}
+
+	@Test
+	public void testRelationshipExclusionByNameRule() throws Exception {
+		String source = "![R]";
+		Rule rule = Rule.from(source);
+		assertNotNull(rule);
+		assertEquals("!R", rule.getDescriptor().getPattern());
+		assertTrue(rule.getDescriptor().getToken(1) instanceof RelToken);
+		RelToken token = (RelToken) rule.getDescriptor().getToken(1);
+		assertEquals(Token.Type.REL, token.getTokenType());
+		assertTrue(token.hasName());
+		assertEquals("R", token.getName());
+		assertFalse(token.hasType());
+	}
+
+	@Test
+	public void testRelationshipExclusionByTypeAndBothNodesRule() throws Exception {
+		String source = "(A)-[:T]-!(B)";
+		Rule rule = Rule.from(source);
+		assertNotNull(rule);
+		assertEquals("N-R-!N", rule.getDescriptor().getPattern());
+		assertTrue(rule.getDescriptor().getToken(0) instanceof NodeToken);
+		assertTrue(rule.getDescriptor().getToken(2) instanceof RelToken);
+		assertTrue(rule.getDescriptor().getToken(5) instanceof NodeToken);
+		NodeToken startToken = (NodeToken) rule.getDescriptor().getToken(0);
+		assertEquals(Token.Type.NODE, startToken.getTokenType());
+		assertTrue(startToken.hasName());
+		assertEquals("A", startToken.getName());
+		RelToken relToken = (RelToken) rule.getDescriptor().getToken(2);
+		assertEquals(Token.Type.REL, relToken.getTokenType());
+		assertFalse(relToken.hasName());
+		assertTrue(relToken.hasType());
+		assertEquals("T", relToken.getType());
+		NodeToken endToken = (NodeToken) rule.getDescriptor().getToken(5);
+		assertEquals(Token.Type.NODE, endToken.getTokenType());
+		assertTrue(endToken.hasName());
+		assertEquals("B", endToken.getName());
+	}
+
+	@Test
+	public void testRelationshipExclusionByTypeAndStartNodeRule() throws Exception {
+		String source = "(A)-[:T]-!()";
+		Rule rule = Rule.from(source);
+		assertNotNull(rule);
+		assertEquals("N-R-!N", rule.getDescriptor().getPattern());
+		assertTrue(rule.getDescriptor().getToken(0) instanceof NodeToken);
+		assertTrue(rule.getDescriptor().getToken(2) instanceof RelToken);
+		assertTrue(rule.getDescriptor().getToken(5) instanceof NodeToken);
+		NodeToken startToken = (NodeToken) rule.getDescriptor().getToken(0);
+		assertEquals(Token.Type.NODE, startToken.getTokenType());
+		assertTrue(startToken.hasName());
+		assertEquals("A", startToken.getName());
+		RelToken relToken = (RelToken) rule.getDescriptor().getToken(2);
+		assertEquals(Token.Type.REL, relToken.getTokenType());
+		assertFalse(relToken.hasName());
+		assertTrue(relToken.hasType());
+		assertEquals("T", relToken.getType());
+		NodeToken endToken = (NodeToken) rule.getDescriptor().getToken(5);
+		assertEquals(Token.Type.NODE, endToken.getTokenType());
+		assertFalse(endToken.hasName());
+	}
+
+	@Test
+	public void testRelationshipExclusionByTypeAndEndNodeRule() throws Exception {
+		String source = "()-[:T]-!(B)";
+		Rule rule = Rule.from(source);
+		assertNotNull(rule);
+		assertEquals("N-R-!N", rule.getDescriptor().getPattern());
+		assertTrue(rule.getDescriptor().getToken(0) instanceof NodeToken);
+		assertTrue(rule.getDescriptor().getToken(2) instanceof RelToken);
+		assertTrue(rule.getDescriptor().getToken(5) instanceof NodeToken);
+		NodeToken startToken = (NodeToken) rule.getDescriptor().getToken(0);
+		assertEquals(Token.Type.NODE, startToken.getTokenType());
+		assertFalse(startToken.hasName());
+		RelToken relToken = (RelToken) rule.getDescriptor().getToken(2);
+		assertEquals(Token.Type.REL, relToken.getTokenType());
+		assertFalse(relToken.hasName());
+		assertTrue(relToken.hasType());
+		assertEquals("T", relToken.getType());
+		NodeToken endToken = (NodeToken) rule.getDescriptor().getToken(5);
+		assertEquals(Token.Type.NODE, endToken.getTokenType());
+		assertTrue(endToken.hasName());
+		assertEquals("B", endToken.getName());
+	}
+
+	@Test
+	public void testLoadingRelationshipExclusionByNameRule() throws Exception {
+		// perform first call to inject relationship
+		String source =
+				"(A) {\"name\": \"Alice\"}\n" +
+				"(B) {\"name\": \"Bob\"}\n" +
+				"(A)-[R:KNOWS]->(B) {\"since\": 1977}\n" +
+				"";
+		Map<String, PropertyContainer> out = GEOFF.loadIntoNeo4j(new StringReader(source), db, null);
+		assertNotNull(out);
+		// build params for second call from output of first
+		Relationship r = (Relationship) out.get("[R]");
+		assertNotNull(r);
+		Map<String, PropertyContainer> params = new HashMap<String, PropertyContainer>(1);
+		params.put("R", r);
+		// make second call to remove relationship
+		source = "![R]";
+		out = GEOFF.loadIntoNeo4j(new StringReader(source), db, params);
+		// check results
+		r = (Relationship) out.get("[R]");
+		assertNull(r);
+	}
+
+	@Test
+	public void testLoadingRelationshipExclusionByTypeAndBothNodesRule() throws Exception {
+		// perform first call to inject relationship
+		String source =
+				"(A) {\"name\": \"Alice\"}\n" +
+				"(B) {\"name\": \"Bob\"}\n" +
+				"(A)-[:KNOWS]->(B) {\"since\": 1977}\n" +
+				"";
+		Map<String, PropertyContainer> out = GEOFF.loadIntoNeo4j(new StringReader(source), db, null);
+		assertNotNull(out);
+		// build params for second call from output of first
+		Node a = (Node) out.get("(A)");
+		assertNotNull(a);
+		Node b = (Node) out.get("(B)");
+		assertNotNull(b);
+		Map<String, PropertyContainer> params = new HashMap<String, PropertyContainer>(2);
+		params.put("A", a);
+		params.put("B", b);
+		// make second call to remove relationship
+		source = "(A)-[:KNOWS]-!(B)";
+		out = GEOFF.loadIntoNeo4j(new StringReader(source), db, params);
+		// check results
+		a = (Node) out.get("(A)");
+		assertNotNull(a);
+		b = (Node) out.get("(B)");
+		assertNotNull(b);
+		assertNull(a.getSingleRelationship(DynamicRelationshipType.withName("KNOWS"), Direction.OUTGOING));
+		assertNull(b.getSingleRelationship(DynamicRelationshipType.withName("KNOWS"), Direction.INCOMING));
+	}
+
+	@Test
+	public void testLoadingRelationshipExclusionByTypeAndStartNodeRule() throws Exception {
+		// perform first call to inject relationship
+		String source =
+				"(A) {\"name\": \"Alice\"}\n" +
+				"(B) {\"name\": \"Bob\"}\n" +
+				"(A)-[:KNOWS]->(B) {\"since\": 1977}\n" +
+				"";
+		Map<String, PropertyContainer> out = GEOFF.loadIntoNeo4j(new StringReader(source), db, null);
+		assertNotNull(out);
+		// build params for second call from output of first
+		Node a = (Node) out.get("(A)");
+		assertNotNull(a);
+		Map<String, PropertyContainer> params = new HashMap<String, PropertyContainer>(1);
+		params.put("A", a);
+		// make second call to remove relationship
+		source = "(A)-[:KNOWS]-!()";
+		out = GEOFF.loadIntoNeo4j(new StringReader(source), db, params);
+		// check results
+		a = (Node) out.get("(A)");
+		assertNotNull(a);
+		assertNull(a.getSingleRelationship(DynamicRelationshipType.withName("KNOWS"), Direction.OUTGOING));
+	}
+
+	@Test
+	public void testLoadingRelationshipExclusionByTypeAndEndNodeRule() throws Exception {
+		// perform first call to inject relationship
+		String source =
+				"(A) {\"name\": \"Alice\"}\n" +
+				"(B) {\"name\": \"Bob\"}\n" +
+				"(A)-[:KNOWS]->(B) {\"since\": 1977}\n" +
+				"";
+		Map<String, PropertyContainer> out = GEOFF.loadIntoNeo4j(new StringReader(source), db, null);
+		assertNotNull(out);
+		// build params for second call from output of first
+		Node b = (Node) out.get("(B)");
+		assertNotNull(b);
+		Map<String, PropertyContainer> params = new HashMap<String, PropertyContainer>(1);
+		params.put("B", b);
+		// make second call to remove relationship
+		source = "()-[:KNOWS]-!(B)";
+		out = GEOFF.loadIntoNeo4j(new StringReader(source), db, params);
+		// check results
+		b = (Node) out.get("(B)");
+		assertNotNull(b);
+		assertNull(b.getSingleRelationship(DynamicRelationshipType.withName("KNOWS"), Direction.INCOMING));
 	}
 
 }
